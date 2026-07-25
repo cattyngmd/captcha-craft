de6f7db8cf8be64d30baefe09cd5bd66f36eec12
cattyngmd <cvenby@gmail.com>
2026-07-26 02:07:31 +0300

    fix(proxy): temporarily resolve authentication issue its bad

---
+++ src/main/java/dev/cattyn/captchacraft/mixin/MixinMain.java
@@ -0,0 +1,17 @@
+package dev.cattyn.captchacraft.mixin;
+
+import net.minecraft.client.main.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Main.class)
+public class MixinMain {
+    // java sucks
+    @Inject(method = "main", at = @At("HEAD"))
+    private static void mainHook(String[] strings, CallbackInfo ci) {
+        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+        System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
+    }
+}
