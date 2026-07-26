67f233c8636a6230011989dd55d8487caa53b277
cattyngmd <cvenby@gmail.com>
2026-07-26 12:40:46 +0300

    docs(proxy): reference jdk 8 release notes

--- src/main/java/dev/cattyn/captchacraft/mixin/MixinMain.java
+++ src/main/java/dev/cattyn/captchacraft/mixin/MixinMain.java
@@ -8,7 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Main.class)
 public class MixinMain {
-    // java sucks
+    // the only way to handle it is to set those props
+    // or use external http client that i don't want to do
+    // https://www.oracle.com/java/technologies/javase/8all-relnotes.html#:~:text=Disable%20Basic%20authentication%20for%20HTTPS%20tunneling
     @Inject(method = "main", at = @At("HEAD"))
     private static void mainHook(String[] strings, CallbackInfo ci) {
         System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
